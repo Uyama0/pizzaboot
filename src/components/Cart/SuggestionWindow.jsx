00557b6e90affdbd763e55a9c9615cc66995d6eb
Ruslan <axelezgames@gmail.com>
2024-01-05 01:03:35 +0500

    add: modal input  window

---
+++ src/components/Cart/SuggestionWindow.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const SuggestionWindow = () => {
+  return (
+    <div className="h-screen w-screen fixed top-0 left-0">
+      {/* <section className="flex justify-center items-center absolute ">
+        <div className="flex">
+          <label htmlFor="">Label</label>
+          <input type="text" />
+        </div>
+      </section> */}
+    </div>
+  );
+};
+
+export default SuggestionWindow;
